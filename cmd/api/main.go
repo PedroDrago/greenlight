1ba871229a9a50a963e034992c9f4976d4d490e9
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-05-27 02:13:33 -0300

    tons of stuff

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const (
@@ -16,6 +20,12 @@ const (
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 }
 
 type application struct {
@@ -24,18 +34,59 @@ type application struct {
 	errorLog *log.Logger
 }
 
-func main() {
-	var cfg config
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
 
+func parseFlags(cfg *config) {
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "DSN for connection with PostgreSQL")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL Max Open Connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL Max Idle Connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL Max Connection Idle Time")
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev | prod | stage)")
 	flag.Parse()
+}
+
+// NOTE:  last page: 135 - Chapter 7 - CRUD Operations
+// FIX:   last page: 135 - Chapter 7 - CRUD Operations
+// TEST:  last page: 135 - Chapter 7 - CRUD Operations
+// WARN:  last page: 135 - Chapter 7 - CRUD Operations
+// TODO:  last page: 135 - Chapter 7 - CRUD Operations
+
+func main() {
+	var cfg config
+
+	parseFlags(&cfg)
 	app := application{
 		config:   cfg,
 		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
 		errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
 	}
+	db, err := openDB(cfg)
+	if err != nil {
+		app.errorLog.Fatal(err)
+	}
+	defer db.Close()
 
+	app.infoLog.Println("Connection with DB established")
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
@@ -44,6 +95,6 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 	app.infoLog.Printf("Starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	app.errorLog.Fatal(err)
 }
