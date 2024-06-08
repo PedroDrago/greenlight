d50465d3daf49c12a75c411d76c01ab33a3c6079
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-06-08 16:01:24 -0300

    partial movie updates

--- cmd/api/movieHandlers.go
+++ cmd/api/movieHandlers.go
@@ -85,10 +85,10 @@ func (app *application) updateMovieHandler(writer http.ResponseWriter, req *http
 	}
 
 	var input struct {
-		Title   string       `json:"title"`
-		Year    int32        `json:"year"`
-		Runtime data.Runtime `json:"runtime"`
-		Genres  []string     `json:"genres"`
+		Title   *string       `json:"title"`
+		Year    *int32        `json:"year"`
+		Runtime *data.Runtime `json:"runtime"`
+		Genres  []string      `json:"genres"`
 	}
 	err = app.readJSON(writer, req, &input)
 	if err != nil {
@@ -96,10 +96,18 @@ func (app *application) updateMovieHandler(writer http.ResponseWriter, req *http
 		return
 	}
 
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
 
 	v := validator.New()
 
