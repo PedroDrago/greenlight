a0fa34b6d8b9fd01b0d561bb433b2d8a192aef66
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-06-08 20:54:00 -0300

    sorting and pagination

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,10 @@
 package data
 
-import "github.com/PedroDrago/greenlight/internal/validator"
+import (
+	"strings"
+
+	"github.com/PedroDrago/greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page         int
@@ -9,10 +13,34 @@ type Filters struct {
 	SortSafelist []string
 }
 
+func (f *Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
+}
+
 func (f *Filters) Validate(v *validator.Validator) {
 	v.Check(f.Page > 0, "page", "must be greater than zero")
 	v.Check(f.Page < 10_000_000, "page", "must be a maximum of 10 million")
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize < 100, "page", "must be a maximum of 100")
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f *Filters) limit() int {
+	return f.PageSize
+}
+
+func (f *Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
 }
